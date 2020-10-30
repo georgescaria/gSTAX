65d560e2c6d1ae2f96cf0687b19991ac2e8ce01f
gscaria1 <sdygeorgescaria@gmail.com>
2020-10-31 03:30:22 +0530

    Updates

--- src/main/java/com/gSTAX/Setup/InitialSetup.java
+++ src/main/java/com/gSTAX/Setup/InitialSetup.java
@@ -1,5 +1,10 @@
 package com.gSTAX.Setup;
 
+import java.sql.Date;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
@@ -20,34 +25,39 @@ public class InitialSetup {
 	
 	public static ExtentReports extent = new ExtentReports();
 	public static ExtentTest Report;
+	public static ExtentSparkReporter spark = new ExtentSparkReporter("Test Results/TestSuite-"+getDateTime()+".html");
 	public static String TC_ID;
 	public static WebDriver driver;
-	
+
+	public static String getDateTime()
+	{
+		Calendar calendar = Calendar.getInstance();
+		Format format = new SimpleDateFormat("dd-mm-yyyy hh-ss");
+		String datetime = format.format(new Date(calendar.getTimeInMillis()));
+		return datetime; 
+	}
 
 	@Parameters({ "TC_ID" })
 	@BeforeTest
 	public static void InitialSetup(String TestCase_ID)
 	{
 		WebDriverManager.chromedriver().setup();
-		
 	    driver = new ChromeDriver();
-		TC_ID=TestCase_ID;
-		ExtentSparkReporter spark = new ExtentSparkReporter("Test Results/Spark.html");
+		TC_ID = TestCase_ID;
 		extent.attachReporter(spark);
 		Report=extent.createTest(TC_ID);
-		
-		
-	
 	}
 	@Test
 	public void extentReportsDemo()
 	{
-	
+
 	}
+
 	@AfterTest
 	public static void endTest()
 	{
-		
-	extent.flush();
+		extent.flush();
+		driver.quit();
+		extent.flush();
 	}
-}
+} 
